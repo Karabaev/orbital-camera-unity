ce0911232159484840d43d5d02188581f7a20278
Karabin <karabaev@reaction.games>
2024-03-17 09:31:02 +0100

    Поддержал изменения в дескрипторах

--- Assets/Runtime/Descriptors/GameCameraConfigSourceBase.cs
+++ Assets/Runtime/Descriptors/GameCameraConfigSourceBase.cs
@@ -1,6 +1,8 @@
-﻿using com.karabaev.descriptors.unity;
+﻿using com.karabaev.descriptors.abstractions.Initialization;
+using com.karabaev.descriptors.unity;
 
 namespace com.karabaev.camera.unity.Descriptors
 {
-  public abstract class GameCameraConfigSourceBase : ScriptableObjectDescriptorRegistrySource<string, GameCameraConfigDescriptor> { }
+  public abstract class GameCameraConfigSourceBase<TProvider> : ScriptableObjectDescriptorRegistrySource<string, GameCameraConfigDescriptor, TProvider> 
+    where TProvider : IDescriptorSourceProvider { }
 }
